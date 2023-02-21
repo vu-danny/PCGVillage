053d95e0e71fe86196b8d8013755b5d27741e4f5
vu-danny <dan.vu.alx@gmail.com>
2023-02-21 20:38:30 +0100

    initial commit

---
+++ Assets/Scripts/StructureType.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StructureType
+{
+    CENTER,
+    BUILDING,
+    ROAD
+}
+
+public enum Decoration
+{
+    PLANT,
+    LIGHTING
+}
